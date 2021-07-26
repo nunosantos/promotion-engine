6cb1cfe5c9bfd13864a15ddaccdc120d77c390ee
Nuno Santos <nunosnddm@gmail.com>
2021-07-26 19:04:32 +0200

    created unit tests to validate that an order can be created

---
+++ tests/Domain-tests/DomainOrderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Domain_tests
+{
+    public class DomainOrderTests
+    {
+        [Fact]
+        public void CreatesOrder_WhenSKUIDIsA_Returns50()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CreatesOrdert_WhenSKUIDIsB_Returns50()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CreatesOrder_WhenSKUIDIsC_Returns20()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CreatesOrder_WhenSKUIDIsD_Returns15()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
