8311b530b99751611899883e0ace1f4298b073ad
Nuno Santos <nunosnddm@gmail.com>
2021-07-26 20:10:32 +0200

    added create item failing integration test and endpoint skeleton

---
+++ src/API/Endpoints/Items/Create.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Endpoints;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Endpoints.Items
+{
+    public class Create : BaseAsyncEndpoint.WithRequest<CreateItemCommand>.WithoutResponse
+    {
+        [HttpPost("items")]
+        [SwaggerOperation(
+            Summary = "Create a set of items",
+            Description = "Create a set of items",
+            OperationId = "Item.Create",
+            Tags = new[] { "ItemEndpoint" })
+        ]
+        public override Task<ActionResult> HandleAsync([FromBody] CreateItemCommand request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Integration-tests/ItemEndpoint/Create.cs
@@ -0,0 +1,27 @@
+﻿using API.Endpoints.Items;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Integration_tests.ItemEndpoint
+{
+
+    public class Create
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        [Fact]
+        public async Task CreateItems()
+        {
+            var createOrderItemsCommand = new CreateItemCommand();
+
+            var stringContent = new StringContent(JsonSerializer.Serialize(createOrderItemsCommand), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync($"/items", stringContent);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
